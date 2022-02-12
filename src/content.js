f31c4f17e379308a3e2d1492da62db930dfc99fd
rhythm191 <sac2ndg+develop@gmail.com>
2022-02-12 16:34:06 +0900

    feat: add timer circle

--- src/content.js
+++ src/content.js
@@ -1,102 +1,67 @@
 (function () {
-  if (document.getElementById("mamemaki-wrapper") !== null) {
-    exitMamemakiMode();
-    return;
+  let wrapper = document.getElementById("timetimer-wrapper");
+
+  if (wrapper !== null) {
+    wrapper.remove();
   }
 
-  // 豆まきの土台を表示
-  const wrapper = document.createElement("div");
-  wrapper.id = "mamemaki-wrapper";
+  wrapper = document.createElement("div");
+  wrapper.id = "timetimer-wrapper";
+  wrapper.style.display = "flex";
+  wrapper.style.flexDirection = "column";
+  wrapper.style.justifyContent = "flex-start";
+  wrapper.style.alignItems = "center";
   wrapper.style.position = "fixed";
-  wrapper.style.left = 0;
+  wrapper.style.left = "calc(100vw - 200px)";
   wrapper.style.top = 0;
-  wrapper.style.width = "100vw";
-  wrapper.style.height = "100vh";
+  wrapper.style.width = "200px";
+  wrapper.style.height = "200px";
   wrapper.style.margin = 0;
   wrapper.style.padding = 0;
-  wrapper.style.cursor = "crosshair";
-  wrapper.style.zIndex = 2323;
+  wrapper.style.zIndex = 3939;
+  wrapper.style.background = "#fff";
+  wrapper.style.border = "1px solid #333";
 
-  // メッセージを表示
-  const message = document.createElement("p");
-  message.id = "mamemaki-message";
-  message.innerHTML = "豆まきモードです。ESCで終了します。";
-  message.style.position = "fixed";
-  message.style.right = 0;
-  message.style.top = 0;
-  message.style.padding = "8px 8px";
-  message.style.margin = 0;
-  message.style.border = "1px solid #000";
-  message.style.background = "#fff";
-  message.style.font = `16px sans-serif;`;
+  // タイマーサークルを生成
+  const circle = document.createElement("div");
+  circle.id = "timetimer-circle";
+  circle.style.boxSizing = "border-box";
+  circle.style.position = "relative";
+  circle.style.padding = "10px 25px";
+  circle.style.width = "150px";
+  circle.style.height = "150px";
+  circle.style.margin = "8px 0 8px";
+  circle.style.borderRadius = "50%";
 
-  wrapper.append(message);
-
-  // クリック時に豆まきをする
-  wrapper.addEventListener("click", function (event) {
-    var soyX = event.offsetX; //X座標
-    var soyY = event.offsetY; //Y座標
-
-    let soyImg = document.createElement("img");
-    soyImg.src = chrome.runtime.getURL(`images/soy.png`);
-    soyImg.style.position = "fixed";
-    soyImg.style.left = `${soyX}px`;
-    soyImg.style.top = `${soyY}px`;
-    soyImg.style.width = "20px";
-    soyImg.style.height = "20px";
-    soyImg.style.transformOrigin = "center";
-
-    soyImg.animate(
-      [
-        { transform: "translate(0, 300px)", offest: 0 },
-        {
-          transform: " translate(0, 0) rotate(45deg)",
-          opacity: 1,
-          offest: 0.9,
-          easing: "cubic-bezier(0.34, 1.56, 0.64, 1)",
-        },
-        {
-          opacity: 0,
-          offest: 1,
-          easing: "cubic-bezier(0.34, 1.56, 0.64, 1)",
-        },
-      ],
-      { duration: 240, fill: "forwards" }
-    );
-
-    wrapper.append(soyImg);
-
-    const effectImg = document.createElement("img");
-    effectImg.src = chrome.runtime.getURL(`images/effect.png`);
-    effectImg.style.position = "absolute";
-    effectImg.style.left = `${soyX - 32}px`;
-    effectImg.style.top = `${soyY - 32}px`;
-    effectImg.style.width = "64px";
-    effectImg.style.height = "64px";
-    effectImg.style.transformOrigin = "center";
-
-    effectImg.animate(
-      { opacity: [1, 0] },
-      { delay: 300, duration: 100, fill: "forwards" }
-    );
-
-    wrapper.append(effectImg);
+  // タイマーの初期角度を計算
+  chrome.storage.sync.get("minutesValue", ({ minutesValue }) => {
+    const minutesDeg = ((60 - minutesValue) / 60) * 360;
+    circle.style.background = `conic-gradient(#fff ${minutesDeg}deg, #fb343c ${minutesDeg}deg 360deg)`;
   });
 
-  document.querySelector("body").append(wrapper);
+  wrapper.append(circle);
 
-  window.addEventListener("keydown", captureEscapeEvent);
+  // closeボタン
+  const close = document.createElement("button");
+  close.id = "timetimer-close";
+  close.innerText = "close";
+  close.style.boxSizing = "border-box";
+  close.style.padding = "4px 8px";
+  close.style.background = "#fff";
+  close.style.border = "1px solid #333";
+  close.style.borderRadius = "4px";
+
+  close.addEventListener("click", () => {
+    wrapper.remove();
+  });
+
+  wrapper.append(close);
+
+  document.querySelector("body").append(wrapper);
 })();
 
-function captureEscapeEvent(event) {
-  if (event.key === "Escape") {
-    exitMamemakiMode();
-  }
-  window.removeEventListener("keydown", captureEscapeEvent);
-}
-
-function exitMamemakiMode() {
-  const wrapper = document.getElementById("mamemaki-wrapper");
+function exiTtimetimerMode() {
+  const wrapper = document.getElementById("timetimer-wrapper");
 
   if (wrapper !== null) {
     wrapper.remove();
--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,20 @@
 const timerForm = document.getElementById("timer-form");
 const minutes = document.getElementById("minutes");
 
-timerForm.addEventListener("submit", function (event) {
+timerForm.addEventListener("submit", async (e) => {
   console.log(minutes);
   console.log(minutes.value);
-  alert(`yeah ${minutes.value}`);
+
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.storage.sync.set({ minutesValue: minutes.value });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"],
+  });
+
+  window.close();
 
   return false;
 });
