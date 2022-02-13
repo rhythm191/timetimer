d2cfdb5e5fb9a86c233c58471eb3bc53c54ba3fb
rhythm191 <sac2ndg+develop@gmail.com>
2022-02-13 17:03:39 +0900

    feat: add animation

--- src/content.js
+++ src/content.js
@@ -1,3 +1,6 @@
+let timerMinutes = 0;
+let startTime = new Date().getTime();
+
 (function () {
   let wrapper = document.getElementById("timetimer-wrapper");
 
@@ -35,8 +38,12 @@
 
   // タイマーの初期角度を計算
   chrome.storage.sync.get("minutesValue", ({ minutesValue }) => {
+    timerMinutes = minutesValue;
+    startTime = new Date().getTime();
     const minutesDeg = ((60 - minutesValue) / 60) * 360;
     circle.style.background = `conic-gradient(#fff ${minutesDeg}deg, #fb343c ${minutesDeg}deg 360deg)`;
+
+    requestAnimationFrame(animationTimer);
   });
 
   wrapper.append(circle);
@@ -60,6 +67,24 @@
   document.querySelector("body").append(wrapper);
 })();
 
+// タイマーのアニメーション
+function animationTimer() {
+  const circle = document.getElementById("timetimer-circle");
+
+  if (circle === null) {
+    return;
+  }
+
+  const now = new Date().getTime();
+  const passedTime = (now - startTime) / 60000;
+  const minutesDeg = ((60 - (timerMinutes - passedTime)) / 60) * 360;
+
+  circle.style.background = `conic-gradient(#fff ${minutesDeg}deg, #fb343c ${minutesDeg}deg 360deg)`;
+
+  requestAnimationFrame(animationTimer);
+}
+
+// タイマー表示を削除する
 function exiTtimetimerMode() {
   const wrapper = document.getElementById("timetimer-wrapper");
 
